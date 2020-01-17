7a5206d4c4ebf6f0613f384f9070a4d71a936f2b
Gerardo Keys <jotojry@icloud.com>
2020-01-16 17:48:24 -0800

    added live header background and started main section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from "react";
 import Header from "./components/Header";
 
 export default function App() {
-    return (
-      <div className="App">
-        <Header />
-        <main>
-          <section className="section-about">Hello World</section>
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <section className="about">
+          <h4 className="about__title--main">Hello World</h4>
+          <h5 className="about__title--sub u-margin-bottom-sm">Hello World</h5>
+          <p className="about__description">
+            Duis cursus dui et commodo porta. Cras vitae facilisis elit. Sed
+            tempor nulla lorem, eget porta magna rhoncus eu. Fusce eget lorem sit
+            amet mi faucibus mattis. Morbi tincidunt suscipit tempus. Proin ut
+            tristique arcu, non vehicula lacus. In hac habitasse platea dictumst.
+            Proin scelerisque tempus commodo. Ut ultrices at erat id suscipit.
+            Aenean sed leo sed magna luctus mollis quis semper lorem. Donec tempor
+            dolor sed tortor mollis egestas. Mauris mi quam, dictum a tortor
+            vitae, placerat facilisis risus. In eu tempus lacus. Phasellus sem
+            enim, ornare eget congue ut, posuere sit amet ligula.
+          </p>
+        </section>
+      </main>
+    </div>
+  );
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import BgVideo from './BgVideo';
 
 export default function Header() {
 
 
     return (
         <header className="header">
+          
           <h1 className="header__primary">
+            <BgVideo />
             <div className="header__container u-margin-bottom-md">
+            
               <div className="icon-box">
                 <img src="#" alt="" className="profile-icon" />
               </div>
