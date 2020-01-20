32b25e414b5d25e6c11017aa00bfc0c16baf64dc
Gerardo Keys <gerardokeys@Gerardos-MacBook-Pro.local>
2020-01-19 16:26:43 -0800

    added video attribute

--- src/components/BgVideo.jsx
+++ src/components/BgVideo.jsx
@@ -5,7 +5,7 @@ import GoldenGateTrafficWeb from "../img/Golden_Gate_Traffic.webm"
 export default function BgVideo() {
     return (
         <div class="bg-video">
-          <video class="bg-video__content" autoPlay muted loop>
+          <video class="bg-video__content" autoPlay muted loop playsInline>
             <source src={GoldenGateTraffic} type="video/mp4" />
             <source src={GoldenGateTrafficWeb} type="video/webm" />
             Your browser is not supported!
