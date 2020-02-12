2310be463d8939e74285088ba01b39ecb04f3172
Gerardo Keys <jotojry@icloud.com>
2020-02-11 17:48:01 -0800

    added logos to projectsfixed some responsive styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,30 +1,44 @@
 import React from "react";
+import GithubDark from "../img/social/github-dark.png";
+import GlobeDark from "../img/social/globe-dark.png";
 
 export default function Projects({ projects }) {
-
   const ProjectCard = ({ data }) => {
     return (
       <div className="projects__container">
-        <i className="projects__icon "></i>
-        <h3 className="heading-tertiary projects__title">
-          {data.title}
-        </h3>
-        <p className="projects__text">
-        {data.description}
-        </p>
-        <div className="projects__screen">
-          <img src={data.screenshot} alt="Webflix site screenshot" className="projects__screen--img"/> 
+        <h3 className="heading-tertiary projects__title">{data.title}</h3>
+
+        <p className="projects__text">{data.description}</p>
+
+        <div className="projects__logo">
+          <img
+            src={GithubDark}
+            className="projects__logo__icon"
+            alt="dark github icon"
+          />
+          <img
+            src={GlobeDark}
+            className="projects__logo__icon"
+            alt="dark globe icon"
+          />
         </div>
-        
+
+        <div className="projects__screen">
+          <img
+            src={data.screenshot}
+            alt="Webflix site screenshot"
+            className="projects__screen--img"
+          />
+        </div>
       </div>
     );
   };
 
   return (
     <section className="projects">
-        {
-            projects.map(data => <ProjectCard data={data} />)
-        }
+      {projects.map(data => (
+        <ProjectCard data={data} />
+      ))}
     </section>
   );
 }
