e1f414b7c4b05d3980b13080f55c0f090e0dd6ac
Gerardo Keys <jotojry@icloud.com>
2020-02-24 13:11:03 -0800

    final touches, will continue working but mvp's have been met

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,22 @@
 import React, { createRef } from "react";
 
 export default function Navigation() {
-
   const checkBoxRef = createRef();
 
   const closeNav = () => {
-    if(checkBoxRef.current.checked) {
-      return checkBoxRef.current.checked = false;
+    if (checkBoxRef.current.checked) {
+      return (checkBoxRef.current.checked = false);
     }
-  }
+  };
 
   return (
     <div className="navigation">
-      <input type="checkbox" className="navigation__checkbox" id="navi-toggle" ref={checkBoxRef} />
+      <input
+        type="checkbox"
+        className="navigation__checkbox"
+        id="navi-toggle"
+        ref={checkBoxRef}
+      />
       <label for="navi-toggle" className="navigation__button">
         <span className="navigation__icon">&nbsp;</span>
       </label>
@@ -22,27 +26,48 @@ export default function Navigation() {
       <nav className="navigation__nav">
         <ul className="navigation__list">
           <li className="navigation__item">
-            <a href="#ABOUT_ANCHOR" className="navigation__link" onClick={() => closeNav()}>
+            <a
+              href="#ABOUT_ANCHOR"
+              className="navigation__link"
+              onClick={() => closeNav()}
+            >
               <span>01</span>About Me
             </a>
           </li>
           <li className="navigation__item">
-            <a href="#EDUCATION_ANCHOR" className="navigation__link" onClick={() => closeNav()}>
+            <a
+              href="#EDUCATION_ANCHOR"
+              className="navigation__link"
+              onClick={() => closeNav()}
+            >
               <span>02</span>Education
             </a>
           </li>
           <li className="navigation__item">
-            <a href="#TECHNOLOGIES_ANCHOR" className="navigation__link" onClick={() => closeNav()}>
+            <a
+              href="#TECHNOLOGIES_ANCHOR"
+              className="navigation__link"
+              onClick={() => closeNav()}
+            >
               <span>03</span>Technologies
             </a>
           </li>
           <li className="navigation__item">
-            <a href="#PROJECTS_ANCHOR" className="navigation__link" onClick={() => closeNav()}>
+            <a
+              href="#PROJECTS_ANCHOR"
+              className="navigation__link"
+              onClick={() => closeNav()}
+            >
               <span>04</span>Projects
             </a>
           </li>
           <li className="navigation__item">
-            <a href="#" className="navigation__link">
+            <a
+              href="https://docs.google.com/document/d/1UBlGvBmSualbOp6Rl3GlLPPDGwneWhl7l4k_01b13WY/edit?usp=sharing"
+              target="_blank"
+              className="navigation__link"
+              onClick={() => closeNav()}
+            >
               <span>05</span>Download Resume
             </a>
           </li>
